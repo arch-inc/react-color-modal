645e3b35bfba3211a2a67bd5d09f3c866150eaa8
arcatdmz <arcatdmz@users.noreply.github.com>
2021-07-15 18:13:32 +0000

    deploy: f0719cd8475a3e8640f79e41e118f115aa3ca9a6

---
+++ _next/static/4LsyBXt515Y5ljHSTkF4Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-917f4ae60ed5edf06377.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-92aa34bb3e1f53da63f6.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
