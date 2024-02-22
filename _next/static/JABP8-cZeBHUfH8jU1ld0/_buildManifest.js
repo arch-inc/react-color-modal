536216d76171507f6ef0d761f2b793465009aa19
arcatdmz <arcatdmz@users.noreply.github.com>
2024-02-22 17:08:46 +0000

    deploy: bab7a1f82b8ba00e5792a5afecfaee25a6810790

---
+++ _next/static/JABP8-cZeBHUfH8jU1ld0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/916-4af378d2118c13ff.js","static/chunks/pages/index-14e2216f59c9ca95.js"],"/_error":["static/chunks/pages/_error-c9810eecf3aa9b73.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/u_7WgV3vcWbL1v0xdO9xB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/501-a41b696db8d99c95.js","static/chunks/pages/index-0357b42cbeef17d5.js"],"/_error":["static/chunks/pages/_error-a4917b9fc0c3a652.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
